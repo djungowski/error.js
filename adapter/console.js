9c811d5eb1d6ca47130f6664d70343818f4dd67a
Dominik Jungowski <Dominik@Jungowski.de>
2012-04-05 16:55:22 +0200

    Console Adapter

---
+++ adapter/console.js
@@ -0,0 +1,12 @@
+ErrorJS.Adapter.Console = function() {
+};
+
+ErrorJS.Adapter.Console.prototype = new ErrorJS.Adapter();
+
+ErrorJS.Adapter.Console.prototype.handleError = function(errorDetails) {
+	var errorMessage;
+
+	errorMessage = 'JavaScript error: "{1}" in "{2}" on line "{3}"';
+	errorMessage = this.formatErrorMessage(errorMessage, errorDetails);
+	console.log(errorMessage);
+};
--- error.js
+++ error.js
@@ -14,8 +14,45 @@ ErrorJS.setOptions = function(options) {
 	}
 };
 
+ErrorJS.adapter = null;
+
+ErrorJS.getAdapter = function() {
+	var adapterName;
+
+	if (ErrorJS.adapter === null) {
+		adapterName = 'ErrorJS.Adapter.';
+		// console => Console
+		adapterName += ErrorJS.options.adapter.charAt(0).toUpperCase() + ErrorJS.options.adapter.slice(1);
+		ErrorJS.adapter = new ErrorJS.Adapter['Console'];
+	}
+	return ErrorJS.adapter;
+};
+
 ErrorJS.handleError = function(message, url, line) {
+	var errorDetails;
+
+	errorDetails = {
+		message: message,
+		url: url,
+		line: line
+	};
+
+	ErrorJS.getAdapter().handleError(errorDetails);
 	return ErrorJS.options.supressErrors;
+};
+
+ErrorJS.Adapter = function() {
+};
+
+ErrorJS.Adapter.prototype.handleError = function(message, url, line) {
+    // Implement Error Handling in subclass here
+};
+
+ErrorJS.Adapter.prototype.formatErrorMessage = function(errorMessage, errorDetails) {
+    errorMessage = errorMessage.replace('{1}', errorDetails.message);
+    errorMessage = errorMessage.replace('{2}', errorDetails.url);
+    errorMessage = errorMessage.replace('{3}', errorDetails.line);
+    return errorMessage;
 };
 
 window.onerror = ErrorJS.handleError;
