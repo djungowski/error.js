9d061fe16a86dacfd60bc30b63dd58aca18d9b17
Dominik Jungowski <Dominik@Jungowski.de>
2012-04-05 17:00:18 +0200

    Ohne quotes

--- adapter/console.js
+++ adapter/console.js
@@ -6,7 +6,7 @@ ErrorJS.Adapter.Console.prototype = new ErrorJS.Adapter();
 ErrorJS.Adapter.Console.prototype.handleError = function(errorDetails) {
 	var errorMessage;
 
-	errorMessage = 'JavaScript error: "{1}" in "{2}" on line "{3}"';
+	errorMessage = 'JavaScript error: "{1}" in "{2}" on line {3}';
 	errorMessage = this.formatErrorMessage(errorMessage, errorDetails);
 	console.log(errorMessage);
 };
